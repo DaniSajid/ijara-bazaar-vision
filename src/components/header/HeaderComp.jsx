5b4b2955cbdb46bfa80e7aeb01a1188156ac180f
Daniyal <dani.play49@gmail.com>
2024-07-02 08:25:13 -0700

    update2

--- src/components/header/HeaderComp.jsx
+++ src/components/header/HeaderComp.jsx
@@ -3,11 +3,12 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import SearchIcon from '@mui/icons-material/Search';
 import AddIcon from '@mui/icons-material/Add';
 import { Link } from "react-router-dom";
+import '../../reusable/props.css'
 const HeaderComp = () => {
   return (
     <div className="header fixed flex aic">
      <LogoComp/>
-     <div className="location rel">
+     <div className="location rel flex aic">
           <SearchIcon className="ico" />
         <input className="label s15 font" placeholder="your location" value="pakistan"/>
         <KeyboardArrowDownIcon className="arro"/>
