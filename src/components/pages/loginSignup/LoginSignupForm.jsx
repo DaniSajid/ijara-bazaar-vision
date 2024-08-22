866c6222feb220d5f24c6c3ede884393d13343c8
Daniyal <dani.play49@gmail.com>
2024-08-22 03:01:08 -0700

    abc

--- src/components/pages/loginSignup/LoginSignupForm.jsx
+++ src/components/pages/loginSignup/LoginSignupForm.jsx
@@ -1,14 +1,35 @@
-import  { useState } from 'react';
-import './loginSignup.css';
-import 'boxicons/css/boxicons.min.css';
+import { useState } from "react";
+import "./loginSignup.css";
+import "boxicons/css/boxicons.min.css";
+import { Link, useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 
 const LoginSignupForm = () => {
+  const navigate = useNavigate();
   const [showSignup, setShowSignup] = useState(false);
   const [showPassword, setShowPassword] = useState({
     login: false,
     signup: false,
     confirmPassword: false,
   });
+  const [loginData, setLoginData] = useState({
+    loginEmail: "",
+    loginPassword: "",
+  });
+  const [signupData, setSignupData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    cPassword: "",
+  });
+  function loginHandleChange(e) {
+    const { name, value } = e.target;
+    setLoginData((prevData) => ({ ...prevData, [name]: value }));
+  }
+  function signupHandleChange(e) {
+    const { name, value } = e.target;
+    setSignupData((prevData) => ({ ...prevData, [name]: value }));
+  }
 
   const toggleForm = () => {
     setShowSignup(!showSignup);
@@ -17,109 +38,200 @@ const LoginSignupForm = () => {
   const togglePasswordVisibility = (field) => {
     setShowPassword({ ...showPassword, [field]: !showPassword[field] });
   };
+  function loginUser(e) {
+    e.preventDefaul();
+    fetch("http://localhost:3000/api/v1/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(loginData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("response data:", data);
 
+        if (data.status === "User created successfully" && data.token) {
+          Cookies.set("token", data.token, { expires: 2 / 24 });
+          alert("Login Successfully");
+          navigate("/");
+        } else {
+          alert(data.message || "Login Failed Please Try Again");
+        }
+      })
+      .catch((err) => {
+        console.log("Fetch ERROR:", err);
+        alert("Fetch Eror: Login Failed");
+      });
+  }
+  function registerUser(e) {
+    e.preventDefault();
+    fetch("http://localhost:3000/api/v1/user/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(signupData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Response Data:", data);
+        alert(data.message);
+        if (data.message === "User created successfully" && data.token) {
+          Cookies.set("token", data.token, { expires: 2 / 24 });
+          navigate("/");
+        }
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+      });
+  }
   return (
     <div className="container">
-    <section className={`container7 forms ${showSignup ? 'show-signup' : ''}`}>
-      <div className="form login">
-        <div className="form-content">
-          <header className='header-css'>Login</header>
-          <form action="#">
-            <div className="field input-field">
-              <input type="email" placeholder="Email" className="input" />
-            </div>
-            <div className="field input-field">
-              <input
-                type={showPassword.login ? 'text' : 'password'}
-                placeholder="Password"
-                className="password"
-              />
-              <i
-                className={`bx ${showPassword.login ? 'bx-show' : 'bx-hide'} eye-icon`}
-                onClick={() => togglePasswordVisibility('login')}
-              ></i>
-            </div>
+      <section
+        className={`container7 forms ${showSignup ? "show-signup" : ""}`}
+      >
+        <div className="form login">
+          <div className="form-content">
+            <header className="header-css">Login</header>
+            <form action="#" onSubmit={loginUser}>
+              <div className="field input-field">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  onChange={loginHandleChange}
+                  name="loginEmail"
+                  className="input"
+                />
+              </div>
+              <div className="field input-field">
+                <input
+                  type={showPassword.login ? "text" : "password"}
+                  placeholder="Password"
+                  className="password"
+                  onChange={loginHandleChange}
+                  name="loginPassword"
+                />
+                <i
+                  className={`bx ${
+                    showPassword.login ? "bx-show" : "bx-hide"
+                  } eye-icon`}
+                  onClick={() => togglePasswordVisibility("login")}
+                ></i>
+              </div>
+              <div className="form-link">
+                <a href="#" className="forgot-pass">
+                  Forgot password?
+                </a>
+              </div>
+              <div className="field button-field">
+                <button>Login</button>
+              </div>
+            </form>
             <div className="form-link">
-              <a href="#" className="forgot-pass">Forgot password?</a>
+              <span>
+                Don't have an account?{" "}
+                <Link to="#" className="link signup-link" onClick={toggleForm}>
+                  Signup
+                </Link>
+              </span>
             </div>
-            <div className="field button-field">
-              <button>Login</button>
-            </div>
-          </form>
-          <div className="form-link">
-            <span>Don't have an account? <a href="#" className="link signup-link" onClick={toggleForm}>Signup</a></span>
+          </div>
+          <div className="line"></div>
+          <div className="media-options">
+            <a href="#" className="field facebook">
+              <i className="bx bxl-facebook facebook-icon"></i>
+              <span>Login with Facebook</span>
+            </a>
+          </div>
+          <div className="media-options">
+            <a href="#" className="field google">
+              <img src="/google.png" alt="" className="google-img" />
+              <span>Login with Google</span>
+            </a>
           </div>
         </div>
-        <div className="line"></div>
-        <div className="media-options">
-          <a href="#" className="field facebook">
-            <i className='bx bxl-facebook facebook-icon'></i>
-            <span>Login with Facebook</span>
-          </a>
-        </div>
-        <div className="media-options">
-          <a href="#" className="field google">
-            <img src="/google.png" alt="" className="google-img" />
-            <span>Login with Google</span>
-          </a>
-        </div>
-      </div>
 
-      <div className="form signup">
-        <div className="form-content">
-          <header className='header-css'>Signup</header>
-          <form action="#">
-            <div className="field input-field">
-              <input type="text" placeholder="Your Name" className="input" />
+        <div className="form signup">
+          <div className="form-content">
+            <header className="header-css">Signup</header>
+            <form action="#" onSubmit={registerUser}>
+              <div className="field input-field">
+                <input
+                  type="text"
+                  placeholder="Your Name"
+                  onChange={signupHandleChange}
+                  name="name"
+                  className="input"
+                />
+              </div>
+              <div className="field input-field">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  onChange={signupHandleChange}
+                  name="email"
+                  className="input"
+                />
+              </div>
+              <div className="field input-field">
+                <input
+                  type={showPassword.signup ? "text" : "password"}
+                  placeholder="Create password"
+                  className="password"
+                  name="password"
+                  onChange={signupHandleChange}
+                />
+                <i
+                  className={`bx ${
+                    showPassword.signup ? "bx-show" : "bx-hide"
+                  } eye-icon`}
+                  onClick={() => togglePasswordVisibility("signup")}
+                ></i>
+              </div>
+              <div className="field input-field">
+                <input
+                  type={showPassword.confirmPassword ? "text" : "password"}
+                  placeholder="Confirm password"
+                  className="password"
+                  name="cPassword"
+                  onChange={signupHandleChange}
+                />
+                <i
+                  className={`bx ${
+                    showPassword.confirmPassword ? "bx-show" : "bx-hide"
+                  } eye-icon`}
+                  onClick={() => togglePasswordVisibility("confirmPassword")}
+                ></i>
+              </div>
+              <div className="field button-field">
+                <button>Signup</button>
+              </div>
+            </form>
+            <div className="form-link">
+              <span>
+                Already have an account?{" "}
+                <Link to="#" className="link login-link" onClick={toggleForm}>
+                  Login
+                </Link>
+              </span>
             </div>
-            <div className="field input-field">
-              <input type="email" placeholder="Email" className="input" />
-            </div>
-            <div className="field input-field">
-              <input
-                type={showPassword.signup ? 'text' : 'password'}
-                placeholder="Create password"
-                className="password"
-              />
-              <i
-                className={`bx ${showPassword.signup ? 'bx-show' : 'bx-hide'} eye-icon`}
-                onClick={() => togglePasswordVisibility('signup')}
-              ></i>
-            </div>
-            <div className="field input-field">
-              <input
-                type={showPassword.confirmPassword ? 'text' : 'password'}
-                placeholder="Confirm password"
-                className="password"
-              />
-              <i
-                className={`bx ${showPassword.confirmPassword ? 'bx-show' : 'bx-hide'} eye-icon`}
-                onClick={() => togglePasswordVisibility('confirmPassword')}
-              ></i>
-            </div>
-            <div className="field button-field">
-              <button>Signup</button>
-            </div>
-          </form>
-          <div className="form-link">
-            <span>Already have an account? <a href="#" className="link login-link" onClick={toggleForm}>Login</a></span>
+          </div>
+          <div className="line"></div>
+          <div className="media-options">
+            <a href="#" className="field facebook">
+              <i className="bx bxl-facebook facebook-icon"></i>
+              <span>Signup with Facebook</span>
+            </a>
+          </div>
+          <div className="media-options">
+            <a href="#" className="field google">
+              <img src="google.png" alt="" className="google-img" />
+              <span>Signup with Google</span>
+            </a>
           </div>
         </div>
-        <div className="line"></div>
-        <div className="media-options">
-          <a href="#" className="field facebook">
-            <i className='bx bxl-facebook facebook-icon'></i>
-            <span>Signup with Facebook</span>
-          </a>
-        </div>
-        <div className="media-options">
-          <a href="#" className="field google">
-            <img src="google.png" alt="" className="google-img" />
-            <span>Signup with Google</span>
-          </a>
-        </div>
-      </div>
-    </section>
+      </section>
     </div>
   );
 };
